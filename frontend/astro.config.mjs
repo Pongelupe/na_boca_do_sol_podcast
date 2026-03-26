23761d333d2720f7c89d483ffd5f553710167d56
pongelupe <pongelupe1998@gmail.com>
2026-03-26 08:48:46 -0300

    ajust astro css

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://nabocadosol.github.io',
+  site: 'https://pongelupe.github.io',
+  base: '/na_boca_do_sol_podcast',
   output: 'static',
 });
